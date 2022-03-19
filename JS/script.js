a48284fa61e9a2714c39d8771c40fad90556955f
Jurzenas <jurzenas737@gmail.com>
2022-03-19 19:19:18 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// let number = 5;
+// const leftNum = 11;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//   a: 50
+// };
+// obj.a = 15;
+// console.log(obj);
+
+// const result = confirm("Are you sure you want?");
+
+// const answer = +prompt("wam jest 18", "18");
+// console.log(answer + 5);
+// console.log(typeof (answer));
+
+const answer = [];
+
+answer[0] = prompt("what is your name", "");
+answer[1] = prompt("what is your surname", "");
+answer[2] = prompt("how old are you", "");
+
+document.write(answer);
+document.write(typeof (answer));
+
+console.log(typeof (answer));
+console.log(typeof (null));
+
