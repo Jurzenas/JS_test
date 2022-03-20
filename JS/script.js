2448773d28b67700835f47838153d0d2f675471c
Jurzenas <jurzenas737@gmail.com>
2022-03-20 19:43:17 +0200

    second commit

--- JS/script.js
+++ JS/script.js
@@ -18,15 +18,22 @@
 // console.log(answer + 5);
 // console.log(typeof (answer));
 
-const answer = [];
+// const answer = [];
 
-answer[0] = prompt("what is your name", "");
-answer[1] = prompt("what is your surname", "");
-answer[2] = prompt("how old are you", "");
+// answer[0] = prompt("what is your name", "");
+// answer[1] = prompt("what is your surname", "");
+// answer[2] = prompt("how old are you", "");
 
-document.write(answer);
-document.write(typeof (answer));
+// document.write(answer);
+// document.write(typeof (answer));
 
-console.log(typeof (answer));
-console.log(typeof (null));
+// console.log(typeof (answer));
+// console.log(typeof (null));
 
+function User(name) {
+  this.name = name;
+  console.log(this);
+}
+
+const newUser = new User("John");
+console.log(this.name);
